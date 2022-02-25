f4121ee2a742721982a20432041a776bf8e0cf2c
minbr0ther <minbr0ther@naver.com>
2022-02-25 15:14:42 +0900

    chore: dependency 설치

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+    </div>
+  )
+}
+
+export default Home
