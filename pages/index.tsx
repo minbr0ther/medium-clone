5242365a8344280f1edcee84771ccade5ac30568
minbr0ther <minbr0ther@naver.com>
2022-02-25 15:32:23 +0900

    feat: banner 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,35 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-0">
+        <div className="space-y-5 px-10">
+          <h1 className="max max-2-xl font-serif text-6xl">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{' '}
+            is a place to write, read, and connect
+          </h1>
+          <h2 className="">
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers
+          </h2>
+        </div>
+        <div>
+          <img
+            className="hidden h-32 object-contain md:inline-flex lg:h-full"
+            src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   )
 }
