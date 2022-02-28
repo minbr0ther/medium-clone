ec853b50f0cb7e78969a5fb177fb53cdfdb2a336
minbr0ther <minbr0ther@naver.com>
2022-02-28 13:40:11 +0900

    feat: post thumbnail  구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity'
 import { Post } from '../typings'
@@ -41,6 +42,33 @@ export default function Home({ posts }: Props) {
       </div>
 
       {/* posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 md:grid-cols-2 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
--- sanity.js
+++ sanity.js
@@ -1,8 +1,6 @@
-import {
-  createImageUrlBuilder,
-  createCurrentUserHook,
-  createClient,
-} from 'next-sanity'
+import { createCurrentUserHook, createClient } from 'next-sanity'
+
+import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
