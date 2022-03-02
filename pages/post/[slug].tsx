f19cc66237af7691596d771a4e5d6ee821358953
minbr0ther <minbr0ther@naver.com>
2022-03-02 13:59:27 +0900

    feat: 상세 Post 페이지 구현 완료 (PortableText)

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,17 +1,65 @@
 import Header from '../../components/Header'
 import { GetStaticProps } from 'next'
-import { sanityClient } from '../../sanity'
+import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
 
 interface Props {
   post: Post
 }
 
 function Post({ post }: Props) {
-  console.log(post)
   return (
     <main>
       <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt=""
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+        <div className="flex items-center space-x-2">
+          <img
+            src={urlFor(post.author.image).url()!}
+            alt=""
+            className="h-10 w-10 rounded-full"
+          />
+          <p className="text-sm font-extralight">
+            post by <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc"> {children} </li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          ></PortableText>
+        </div>
+      </article>
     </main>
   )
 }
@@ -71,5 +119,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     }
   }
 
-  return { props: { post } }
+  // after 60 seconds, it'll update the old cached version
+  return { props: { post }, revalidate: 60 }
 }
