c0cfea3761ea299149e322ce12c3c22fec70cc01
minbr0ther <minbr0ther@naver.com>
2022-03-02 16:31:04 +0900

    feat: 댓글 구현 완료

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -170,6 +170,19 @@ function Post({ post }: Props) {
           />
         </form>
       )}
+
+      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow-lg">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+        {post.comments.map((comment) => (
+          <div key={comment._id} className="">
+            <p>
+              <span className="text-yellow-500">{comment.name}</span>:
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
